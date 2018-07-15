7b92c820c47166151ff5440f25a826ff3baf8116
dess1e <happy.mooshroom@gmail.com>
2018-07-15 16:36:36 +0300

    code refactoring

--- Shaders/simple/fs.glsl
+++ Shaders/simple/fs.glsl
@@ -4,9 +4,8 @@ out vec4 frag_color;
 in vec2 tex_coords;
 uniform sampler2D texture_diffuse1;
 
-uniform float time;
 
 void main()
 {
-    frag_color = texture(texture_diffuse1, tex_coords) * cos(time);
+    frag_color = texture(texture_diffuse1, tex_coords);
 }
